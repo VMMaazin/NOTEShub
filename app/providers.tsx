60f283700cbe492b7a53a91af735ee11c95d32be
MAAZIN <maazinmuhammed0@gmail.com>
2026-02-28 17:33:36 +0530

    Error Correction

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,8 +4,6 @@ import { ThemeProvider } from "next-themes";
 import { AuthProvider } from "@/context/AuthContext";
 import Navbar from "@/components/Navbar";
 import { Toaster } from "sonner";
-import { AnimatePresence, motion } from "framer-motion";
-import { usePathname } from "next/navigation";
 import BackToTop from "@/components/BackToTop";
 
 export default function Providers({
@@ -13,24 +11,13 @@ export default function Providers({
 }: {
   children: React.ReactNode;
 }) {
-  const pathname = usePathname();
-
   return (
     <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
       <AuthProvider>
         <Navbar />
-        <AnimatePresence mode="wait">
-          <motion.main
-            key={pathname}
-            initial={{ opacity: 0, y: 10 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: -10 }}
-            transition={{ duration: 0.3 }}
-            className="flex-1"
-          >
-            {children}
-          </motion.main>
-        </AnimatePresence>
+        <main className="flex-1">
+          {children}
+        </main>
         <BackToTop />
         <Toaster position="bottom-right" richColors />
       </AuthProvider>
