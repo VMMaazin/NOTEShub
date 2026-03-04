d500d69dd5a3f3ce73391af482edb707e975c84b
MAAZIN <maazinmuhammed0@gmail.com>
2026-03-04 17:36:35 +0530

    Added Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import Providers from "./providers";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,8 +20,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${inter.className} min-h-screen bg-background antialiased selection:bg-primary/20 selection:text-primary`}>
+      <body
+        className={`${inter.className} min-h-screen bg-background antialiased selection:bg-primary/20 selection:text-primary`}
+      >
         <Providers>{children}</Providers>
+        <Analytics />
       </body>
     </html>
   );
